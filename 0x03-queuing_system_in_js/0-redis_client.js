f7f40b1856d61b574eaaac0a5d59c9cb7fe5ccd3
eyasuyid <eyasuyid@gmail.com>
2023-03-01 17:20:02 +0300

    removed

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,6 @@
 import { createClient } from 'redis';
 
-const client = createClient({
-  socket: {
-    host: '127.0.0.1',
-    port: 6379
-  }
-});
+const client = createClient();
 
 client.on('error', err => {
   console.log(err);
