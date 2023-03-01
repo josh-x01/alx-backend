f4f93f3321683453ff72366c282a70f53cf79c90
eyasuyid <eyasuyid@gmail.com>
2023-03-01 17:37:25 +0300

    set and get value

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', err => {
+  console.log('Redis client not connected to the server: ' + err.message);
+});
+
+client.on('connect', function () {
+  console.log('Redis client connected to the server');
+})
+
+function setNewSchool (schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue (schoolName) {
+  client.get(schoolName, (err, value) => {
+    console.log(value);
+  } );
+}
+
+// calling methods
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
